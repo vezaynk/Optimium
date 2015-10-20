caae93f863c578a18fc750be5778f66fe4b8bbea
Slava Knyazev <slava@knyz.org>
2015-10-19 23:13:53 -0400

    Added installer

---
+++ install.sh
@@ -0,0 +1,24 @@
+
+curpath=`pwd`
+if ! [[ $curpath =~ .*/ghost/content/themes.* ]]
+then
+   echo "Install path is not valid."
+   echo "Move to you themes directory (e.g. 
+/path/to/ghost/content/themes)"
+   exit 1
+fi
+
+echo "Installation started..."
+echo "Removing any previous Optimium installs..."
+rm -rf Optimium
+echo "Downloading Optimium from GitHub..."
+wget -q https://github.com/viruzx/Optimium/archive/master.zip
+echo "Extracting Archive..."
+unzip -q master.zip
+rm master.zip
+mv Optimium-master Optimium
+service ghost restart << /dev/null
+echo "Done!"
+echo "You may need to now restart Ghost to have it as an option."
+echo "Run this script occasionally to make sure you have the latest 
+build."
