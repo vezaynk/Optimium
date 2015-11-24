26c635f387c1094bf3dda31f48249c8bfa9044ab
Slava Knyazev <slava@knyz.org>
2015-11-23 23:42:31 -0500

    Added smooth scrolling effect (Awesome)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,30 @@ tiles.each(function(i){
 	if(a<b || c<b)
 		$(this).fadeTo(500,1);
 	});
+jQuery.extend(jQuery.easing, {
+    easeOutQuint: function(x, t, b, c, d) {
+        return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+    }
+});
+
+var wheel = false,
+    $docH = $(document).height() - $(window).height(),
+    $scrollTop = $(window).scrollTop();
+
+$(window).bind('scroll', function() {
+    if (wheel === false) {
+        $scrollTop = $(this).scrollTop();
+    }
+});
+$(document).bind('DOMMouseScroll mousewheel', function(e, delta) {
+    delta = delta || -e.originalEvent.detail / 3 || e.originalEvent.wheelDelta / 60;
+    wheel = true;
+
+    $scrollTop = Math.min($docH, Math.max(0, parseInt($scrollTop - delta * 30)));
+    $("body").stop().animate({
+        scrollTop: $scrollTop + 'px'
+    }, 1000, 'easeOutQuint', function() {
+        wheel = false;
+    });
+    return false;
+});
