53629db4f171ddece607f67c1d522c93ca523cf0
Slava Knyazev <slava@knyz.org>
2015-11-27 00:21:17 -0500

    Added arrow to bottom of header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -43,3 +43,18 @@ $(document).bind('DOMMouseScroll mousewheel', function(e, delta) {
     });
     return false;
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
